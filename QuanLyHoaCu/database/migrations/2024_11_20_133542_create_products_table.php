42e131b6afc0b759720effd99075fd9699be06bf
Hong.Thuy <thuypd15122003@gmail.com>
2024-11-20 22:51:30 +0700

    update migrations

---
+++ QuanLyHoaCu/database/migrations/2024_11_20_133542_create_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->string('product_name', 100);
+            $table->string('description', 200)->nullable();
+            $table->integer('price');
+            $table->integer('quantity');
+            $table->string('unit', 50);
+            $table->string('image', 100);
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('category_id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
